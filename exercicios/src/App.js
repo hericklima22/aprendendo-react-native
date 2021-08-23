2959afb31d58bc14775fb3f2955940fd39afb6b8
hericklima22 <herick.lima77@gmail.com>
2021-08-23 20:27:58 -0300

    aula 20 do curso de react native

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react' //sempre que usar um jsx eu preciso importar o React
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 import Primeiro from './components/Primeiro'
+import X, { Comp1, Comp2} from './components/Multi'
 
 export default () => (
     <View>
+        <X />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
     </View>
 )
