c00aa92f0828492aa65a06c3b76d6ee7b0af10c9
hericklima22 <herick.lima77@gmail.com>
2021-08-27 18:04:19 -0300

    arquivo app.js modificado

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'; //sempre que usar um jsx eu preciso importar o React
 import {View, StyleSheet} from 'react-native';
 
+import MinMax from "./components/MinMax";
 import Primeiro from './components/Primeiro';
 import CompPadrao, {Comp1, Comp2} from './components/Multi';
 
 export default () => (
   <View style={style.App}>
-    <CompPadrao />
+    <MinMax min={3} max={20} />
+    <MinMax min={5} max={94} />
+    {/* <CompPadrao />
     <Comp1 />
     <Comp2 />
-    <Primeiro />
+    <Primeiro /> */}
   </View>
 );
 
@@ -17,7 +20,8 @@ const style = StyleSheet.create({
   App: {
     backgroundColor: '#c4c4c4',
     flexGrow: 1,
-    justifyContent: "center", 
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
   },
 });
