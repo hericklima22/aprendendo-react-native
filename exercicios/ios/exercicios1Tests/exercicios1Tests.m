8287bfd7014026ab71189d1d547a68eac136cbc3
hericklima22 <herick.lima77@gmail.com>
2021-08-27 18:21:14 -0300

    arquivos ios deletados

--- exercicios/ios/exercicios1Tests/exercicios1Tests.m
+++ exercicios/ios/exercicios1Tests/exercicios1Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface exerciciosTests : XCTestCase
+@interface exercicios1Tests : XCTestCase
 
 @end
 
-@implementation exerciciosTests
+@implementation exercicios1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
